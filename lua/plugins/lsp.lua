cbffe42c4e896c855b842ad28ac6b5de7880a66e
Donovan Palmer <donovan@donovanpalmer.net>
2025-05-26 14:01:29 -0700

    hiding tailwind @ rule warnings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,7 +23,22 @@ return {
       inlay_hints = { enabled = false },
       ---@type lspconfig.options
       servers = {
-        cssls = {},
+        cssls = {
+          settings = {
+            css = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            scss = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+          },
+        },
         tailwindcss = {
           root_dir = function(...)
             return require("lspconfig.util").root_pattern(".git")(...)
